53c6cb91694358163736ed7e88391299ece83a04
Daniel Cordova <jdany0809@hotmail.com>
2022-04-20 14:30:27 -0500

    new routing structure added

---
+++ src/routes/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
+import RecoveryPassword from "../containers/RecoveryPassword";
+import "../styles/global.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/recovery-password" element={<RecoveryPassword />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
